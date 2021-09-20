a31d9676ba6235ab4760957db65b8aab40c053bb
Ever <evermedina2025@gmail.com>
2021-09-19 22:30:24 -0700

    Route to server.js file

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.static("public"));
 // MongoDB Connection
 mongoose.connect(process.env.MONGOBD_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
 
-
+//Routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port http://localhost:${PORT}!`);
